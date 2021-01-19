189650e386208b99e807c0910a260f97cab4a0d3
PiotrGrzy <piotr.grzymowicz1@gmail.com>
2021-01-19 15:35:20 +0100

    change env name

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: process.env.GATSBY_API_URL,
+  baseURL: process.env.GATSBY_USERS_SERVER_URL,
   withCredentials: true,
   timeout: 10 * 1000,
 })
